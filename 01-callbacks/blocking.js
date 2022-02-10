77eb9341e3862e5f06351b5914df521bb5f3c06b
JhuderPalomino <jhuder.palomino.27@unsch.edu.pe>
2022-02-09 22:29:33 -0500

    feat: first commit with callbacks

---
+++ 01-callbacks/blocking.js
@@ -0,0 +1,18 @@
+const { getUsuarioSync } = require('./users');
+
+console.log('Inicio de programa');
+console.time('inicio');
+
+
+const usuario1 = getUsuarioSync( 1 );
+console.log('Usuario 1:', usuario1 );
+
+
+console.log('Fin de programa');
+const usuario2 = getUsuarioSync( 2 );
+console.log('Usuario 2:', usuario2 );
+
+
+
+console.log('Fin de programa');
+console.timeEnd('inicio');
