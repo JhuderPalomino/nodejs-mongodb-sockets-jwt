0e1aef2b7e542ed775b75efc2295ff9c1d0cac38
JhuderPalomino <jhuder.palomino.27@unsch.edu.pe>
2022-02-13 09:06:15 -0500

    feat: add template

---
+++ 02-fundamentos/template.js
@@ -0,0 +1,7 @@
+const nombre = "JHUDER";
+const apellido = "PALOMIINO MATIAS"
+
+const normal = nombre + ' ' + apellido;
+const template = `${nombre} ${apellido}`
+console.log(normal);
+console.log(template);
