875973354f0c57a1877657582e4891b230c1274a
JhuderPalomino <jhuder.palomino.27@unsch.edu.pe>
2022-02-16 22:59:48 -0500

    refactor: async-await code

--- 02-fundamentos/async-await.js
+++ 02-fundamentos/async-await.js
@@ -31,7 +31,7 @@ const getEmpleado = (id) => {
 
         (empleado)
             ? resolve(empleado)
-            : reject(`No existe el empleado con id ${id}`)
+            : reject(`No existe el empleado con ID ${id}`)
 
     })
 
@@ -39,11 +39,38 @@ const getEmpleado = (id) => {
 const getSalario = (id) => { 
     return new Promise ((resolve, reject) => { 
         const salario = salarios.find(s => s.id === id)?.monto;
-
+        const empleado = empleados.find(e => e.id === id)?.nombre;
         (salario)
             ? resolve(salario)
-            : reject(`no tiene salario`)
+            : reject(` ${empleado} no tiene salario`)
 
     })
 
 }
+
+getEmpleadosInfo = async(id)  =>{
+
+    try{
+        const empleado = await getEmpleado(id);
+        const salario = await getSalario(id);
+
+        return `El salario del empleado ${empleado} es de ${salario}`;
+    }catch (error) {
+        return error;
+        // throw error;
+    }
+
+
+}
+
+const id=3;
+
+getEmpleadosInfo(id)
+    .then(msg=>{
+        console.log('TODO BIEN')
+        console.log(msg)})
+    .catch(err=> {
+        console.log('TODO MAL')
+        console.log(err) }
+    );
+
