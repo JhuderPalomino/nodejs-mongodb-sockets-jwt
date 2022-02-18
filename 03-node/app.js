528cd0b7e16ec4a8e2c948292449492372931ac3
JhuderPalomino <jhuder.palomino.27@unsch.edu.pe>
2022-02-18 11:02:23 -0500

    feat: add app.js

---
+++ 03-node/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+console.clear();
+const base = 3;
+
+let data ='';
+for (let i = 0; i <= 10; i++) {
+    data += `${base} * ${i} = ${base * i}\n`;
+}
+
+fs.writeFile("./test.txt", data, function (err) {
+    (err)
+        ?console.log(err)
+        :console.log("El archivo fue guardado!\n");
+});
+
+fs.readFile('./test.txt', 'utf8', (err, data) => {
+    (err)
+        ? console.log(err)
+        : console.log(`Datos:\n${data}`);
+});
+
